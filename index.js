5915e29662041ce29e71365fff44ffffdb3b30bf
Tobias Reigenborn <tobias.reigenborn@gmail.com>
2021-11-22 20:08:50 -0300

    added navbar and filters in mobile and tablet view

---
+++ index.js
@@ -0,0 +1,49 @@
+const seeMore = document.querySelector('.see-more');
+
+seeMore.addEventListener('click', (e) => {
+  const extraFilters = document.getElementById('extra-filters');
+  const asideOptionsDiv = document.getElementsByClassName('aside-options')[0];
+  const seeMoreButton = document.getElementsByClassName('see-more')[0];
+
+  if (extraFilters.style.display === 'none') {
+    extraFilters.style.display = 'block';
+    asideOptionsDiv.style.borderBottom = 'none';
+    seeMoreButton.innerHTML = 'See Less';
+  } else {
+    extraFilters.style.display = 'none';
+    asideOptionsDiv.style.borderBottom = '1px solid var(--border-grey)';
+    seeMoreButton.innerHTML = 'See More';
+  }
+});
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+hamburgerMenu.addEventListener('click', (e) => {
+  const mobileNav = document.getElementsByClassName(
+    'navbar-bottom-categories'
+  )[0];
+  if (mobileNav.style.display === 'none') {
+    mobileNav.style.display = 'block';
+    hamburgerMenu.children[0].src = './assets/icons/close-menu.png';
+  } else {
+    mobileNav.style.display = 'none';
+    hamburgerMenu.children[0].src = './assets/icons/hamburger-menu.png';
+  }
+});
+
+const filterBy = document.querySelector('.filter-by');
+
+filterBy.addEventListener('click', (e) => {
+  const filters = document.getElementsByClassName('filters')[0];
+  if (filters.style.display === 'none') {
+    filters.style.display = 'block';
+  } else {
+    filters.style.display = 'none';
+  }
+});
+
+const closeFilters = document.querySelector('#close-filters');
+
+closeFilters.addEventListener('click', (e) => {
+  document.getElementsByClassName('filters')[0].style.display = 'none';
+});
